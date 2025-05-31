6c95b94dce54adc3a18a2e4a312a84a61c95e273
CRAZE-LABES <mehetab277@gmail.com>
2025-05-31 16:15:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-
 /**
- * @fileoverview Main entry file for the Hydra Daemon. This module sets up an
+ * @fileoverview Main entry file for the CrazePanel Daemon. This module sets up an
  * Express server integrated with Docker for container management and WebSocket for real-time communication.
  * It includes routes for instance management, deployment, and power control, as well as WebSocket endpoints
  * for real-time container stats and logs. Authentication is enforced using basic authentication.
@@ -148,7 +147,7 @@ function initializeWebSocketServer(server) {
 
                 switch (msg.event) {
                     case 'cmd':
-                        // Do absolutely fucking nothing. Not handling it here.
+                        // Do absolutely nothing. Not handling it here.
                         break;
                     case 'power:start':
                         performPowerAction(ws, container, 'start');
@@ -172,7 +171,7 @@ function initializeWebSocketServer(server) {
         function authenticateWebSocket(ws, req, password, callback) {
             if (password === config.key) {
                 log.info('successful authentication on ws');
-                ws.send(`\r\n\u001b[33m[draco] \x1b[0mconnected!\r\n`);
+                ws.send(`\r\n\u001b[33m[CrazePanel] \x1b[0mconnected!\r\n`);
                 const urlParts = req.url.split('/');
                 const containerId = urlParts[2];
                 const volumeId = urlParts[3] || 0;
@@ -288,7 +287,7 @@ function initializeWebSocketServer(server) {
                 });
         
                 stream.on('end', () => {
-                    ws.send('\u001b[1m\u001b[33mcontainer@draco~ \u001b[0mServer marked as Offline\r\n');
+                    ws.send('\u001b[1m\u001b[33mcontainer@craze~ \u001b[0mServer marked as Offline\r\n');
                 });
         
                 stream.on('error', (err) => {
@@ -313,17 +312,17 @@ function initializeWebSocketServer(server) {
             };
         
             if (!actionMap[action]) {
-                ws.send(`\r\n\u001b[33m[draco] \x1b[0Invalid action: ${action}\r\n`);
+                ws.send(`\r\n\u001b[33m[CrazePanel] \x1b[0Invalid action: ${action}\r\n`);
                 return;
             }
         
-            ws.send(`\r\n\u001b[33m[draco] \x1b[0mWorking on ${action}...\r\n`);
+            ws.send(`\r\n\u001b[33m[CrazePanel] \x1b[0mWorking on ${action}...\r\n`);
         
             try {
                 await actionMap[action]();
             } catch (err) {
                 console.error(`Error performing ${action} action:`, err);
-                ws.send(`\r\n\u001b[33m[HydraD] \x1b[0Action failed: ${err.message}\r\n`);
+                ws.send(`\r\n\u001b[33m[CrazePanel] \x1b[0Action failed: ${err.message}\r\n`);
             }
         }
 
@@ -384,7 +383,7 @@ app.get('/', async (req, res) => {
         // Prepare the response object with Docker status
         const response = {
             versionFamily: 1,
-            versionRelease: 'draco ' + config.version,
+            versionRelease: 'CrazePanel ' + config.version,
             online: true,
             remote: config.remote,
             mysql: {
@@ -401,7 +400,7 @@ app.get('/', async (req, res) => {
         res.json(response); // the point of this? just use the ws - yeah conn to the ws on nodes page and send that json over ws
     } catch (error) {
         console.error('Error fetching Docker status:', error);
-        res.status(500).json({ error: 'Docker is not running - Craze will not function properly.' });
+        res.status(500).json({ error: 'Docker is not running - CrazePanel will not function properly.' });
     }
 });
 
@@ -418,6 +417,6 @@ app.use((err, req, res, next) => {
 const port = config.port;
 setTimeout(function (){
   server.listen(port, () => {
-    log.info('Draco is listening on port ' + port);
+    log.info('CrazePanel is listening on port ' + port);
   });
 }, 2000);
